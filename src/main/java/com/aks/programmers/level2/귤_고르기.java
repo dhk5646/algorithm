f5a47c176b42e1d4bc0a73ff97d9bd99a4dfc794
dhk5646@gmail.com <>
2024-08-19 22:40:12 +0900

    귤 고르기

---
+++ src/main/java/com/aks/programmers/level2/귤_고르기.java
@@ -0,0 +1,16 @@
+package com.aks.programmers.level2;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/138476
+ */
+public class 귤_고르기 {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        
+        return answer;
+    }
+
+}
