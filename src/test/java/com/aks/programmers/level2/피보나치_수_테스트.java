0a75f6bbf34da31f34312c841b0033ddf9e40d08
동구리 <80397165+dhk5646@users.noreply.github.com>
2024-11-13 22:18:30 +0900

    feature/12945 (#9)
    Co-authored-by: dhk5646@gmail.com <>

---
+++ src/test/java/com/aks/programmers/level2/피보나치_수_테스트.java
@@ -0,0 +1,48 @@
+package com.aks.programmers.level2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class 피보나치_수_테스트 {
+
+    private final 피보나치_수 instance = new 피보나치_수();
+
+    @Test
+    public void testSolution() {
+        // given
+        int n = 3;
+
+        // when
+        int expected = 2;
+        int actual = instance.solution(n);
+
+        // then
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSolution2() {
+        // given
+        int n = 5;
+
+        // when
+        int expected = 5;
+        int actual = instance.solution(n);
+
+        // then
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSolution3() {
+        // given
+        int n = 1500;
+
+        // when
+        int expected = 1058440;
+        int actual = instance.solution(n);
+
+        // then
+        Assertions.assertEquals(expected, actual);
+    }
+}
